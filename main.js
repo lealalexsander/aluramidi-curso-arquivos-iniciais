f9650d91e316e3b6a0d2a56350176a842c9c9a74
Alexsander Leal <alexsanderunr@gmail.com>
2024-08-21 15:32:47 -0300

    keydown e keyup javascript

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 function tocaSom(idElementAudio) {
-    document.querySelector(idElementAudio).play();  
-}
+    const elemento = document.querySelector(idElementAudio);  
+
+    if (elemento == null){
+        alert("Elemento não encontrado!!");
+    } else if (elemento.localName === 'audio') {
+        elemento.play();
+    }else {
+        alert("Elemento não é audio!!");
+    }
+}   
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
-    const tecla = listaDeTeclas[contador];
+for (let i = 0; i < listaDeTeclas.length; i++){
+    const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
@@ -15,5 +22,14 @@ while (contador < listaDeTeclas.length) {
         tocaSom(idAudio);
     }
 
-    contador ++;    
+    tecla.onkeydown = function (evento) {
+        console.log(evento.code);
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }         
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
